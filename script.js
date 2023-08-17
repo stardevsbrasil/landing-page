e6b7223fe5b2691823b114e70c12f08b41a09eb0
alvaroeduardo <alvaroedusilva@yahoo.com>
2023-08-17 18:48:17 -0300

    landing page completa.

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttonForm = document.getElementById("form");
+
+function abrirFormulario() {
+    window.open("https://forms.gle/Lygktn9FkMvs6i6h8", "_blank")
+}
+
+console.log(`Você é apaixonado por inovação e tecnologia? Quer fazer parte de uma equipe criativa e dinâmica que transforma ideias em soluções digitais incríveis? Junte-se a nós na StarDevs e faça parte da nossa jornada de sucesso! Explore oportunidades emocionantes e traga sua expertise para moldar o futuro da tecnologia. Vamos construir juntos o próximo grande projeto. Candidate-se hoje e seja parte da revolução digital! 🚀💼
+
+Acesse o link: https://forms.gle/ifGyhhMLsQaPrza37
+`);
